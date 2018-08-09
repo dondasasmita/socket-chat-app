e592c8b3290bb745040a5ae716d338e53b23bb70
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-08-09 13:54:38 +0700

    setup express app and render public directory

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 const path = require("path");
+const express = require("express");
 
+// Create App
+const app = express();
+// Path to public directory
 const publicPath = path.join(__dirname, "../public");
+// Port
+const port = process.env.PORT || 3000;
 
-console.log(publicPath);
+// Render index html
+app.get("/", (req, res) => {
+  res.sendFile(publicPath + "/index.html");
+});
+
+// App listening on port 3000
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
